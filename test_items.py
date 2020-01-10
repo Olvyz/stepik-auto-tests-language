57ce3e1e73ccd4fb3448246d20a7c0811927eab8
Olha Vyzhevska <42007011+Olvyz@users.noreply.github.com>
2020-01-10 17:23:59 +0100

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@ import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_find_button_add(browser):
+def test_find_button_add_to_basket(browser):
     browser.get(link)
     time.sleep(30)
     button = browser.find_element_by_xpath("//*[@id='add_to_basket_form']/button[@type='submit']")
