567c53f3c012ab0b9493ceb665ea4ee80bf2d275
Olha Vyzhevska <ovizhevska@gmail.com>
2020-01-10 16:41:57 +0100

    homework to module 3.6 added

---
+++ test_items.py
@@ -0,0 +1,11 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_find_button_add(browser):
+    browser.get(link)
+    time.sleep(30)
+    button = browser.find_element_by_xpath("//*[@id='add_to_basket_form']/button[@type='submit']")
+    assert button.is_displayed(), "Button not found"
+
+
